ce256e7f983ef0082278ac0b13d57fea736b01c0
Md. Habibur Rahaman <34125799+nobel7761@users.noreply.github.com>
2022-07-31 11:45:44 +0600

    Update ReactCode.tsx

--- apps/builder/components/share/codeSnippets/ReactCode.tsx
+++ apps/builder/components/share/codeSnippets/ReactCode.tsx
@@ -22,7 +22,7 @@ export const StandardReactDiv = ({
     parseContainerSnippet({
       url: `${
         isEmpty(env('VIEWER_INTERNAL_URL'))
-          ? env('VIEWER_URL')
+          ? process.env.NEXT_PUBLIC_VIEWER_URL
           : env('VIEWER_INTERNAL_URL')
       }/${typebot?.publicId}`,
       heightLabel,
