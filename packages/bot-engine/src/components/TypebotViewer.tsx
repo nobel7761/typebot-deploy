31b8d4b92849232f4b7861edf235e449bdb4c3cc
Md. Habibur Rahaman <34125799+nobel7761@users.noreply.github.com>
2022-07-31 12:01:52 +0600

    Update TypebotViewer.tsx

--- packages/bot-engine/src/components/TypebotViewer.tsx
+++ packages/bot-engine/src/components/TypebotViewer.tsx
@@ -41,7 +41,7 @@ export type TypebotViewerProps = {
 
 export const TypebotViewer = ({
   typebot,
-  apiHost = process.env.NEXT_PUBLIC_VIEWER_URL,
+  apiHost = process.env.NEXT_PUBLIC_VIEWER_URL?.split(',')[0],
   isPreview = false,
   isLoading = false,
   style,
